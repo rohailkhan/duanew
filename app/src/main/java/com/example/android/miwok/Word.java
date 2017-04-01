c5e94b69d96d8617f185be6c2f5be8bb63b95a25
Rohl Khan <rohailkhan@gmail.com>
2017-04-01 17:20:49 +1300

    Added all four views and their class i.e introduction,one to 20,one to forty and Asaaib

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
+
+    /** Default translation for the word */
+    private String arabic;
+
+    /** Miwok translation for the word */
+    private String translation;
+    private String ref;
+
+    public Word(String arabic, String translation, String ref) {
+        this.arabic = arabic;
+        this.translation = translation;
+        this.ref = ref;
+    }
+
+    /**
+     * Create a new Word object.
+     *
+
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getArabic() {
+        return arabic;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getTranslation() {
+        return translation;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+}
